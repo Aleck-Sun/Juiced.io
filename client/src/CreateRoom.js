f3a031b47a1ae25627cf4d3b42fef647adfc1a22
arakush <arakush@sfu.ca>
2022-02-18 20:19:55 -0800

    Added components like Home, Welcome, CreateRoom, and added CSS styles

---
+++ client/src/CreateRoom.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./styles.css";
+
+export default function CreateRoom() {
+    return (
+        <div className="CreateRoom">
+            <h1>
+                Would you like to create a room or join a room?
+            </h1>
+        </div>
+    );
+}
---
+++ client/src/Welcome.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./styles.css";
+
+export default function Welcome() {
+    return (
+        <div className="Welcome">
+            <h1>
+                Welcome back!
+            </h1>
+        </div>
+    );
+}
