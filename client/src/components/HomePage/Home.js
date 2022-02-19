6dadf441417b8ee2a5f62b2441ca04090252e66a
Aleck Bowen Sun <aleck.bowen.sun@gmail.com>
2022-02-19 00:20:18 -0500

    Styling changes

--- client/src/components/HomePage/Home.js
+++ client/src/components/HomePage/Home.js
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import homeImg from '../../images/home.png';
 import "../../styles.css";
 
 export default function HomePage() {
     const navigate = useNavigate();
     
     return (
-        <div className="Home">
+        <div className="d-flex flex-column d-flex align-items-center">
             <h1>
                 Ready to battle?
             </h1>
-            <button className="Create">
-                Create Game
-            </button>
-            <button className="Join">
-                Join Game
-            </button> 
-            <button onClick={() => navigate('/instructions')}>
-                How do I play?
-            </button>           
+            <img src={homeImg} />
+            <div className="d-flex justify-content-center">
+                <button className="btn btn-primary m-2">
+                    Create Game
+                </button>
+                <button className="btn btn-primary m-2">
+                    Join Game
+                </button> 
+                <button className="btn btn-primary m-2" onClick={() => navigate('/instructions')}>
+                    How do I play?
+                </button>
+            </div>           
         </div>
     );
 }
--- client/src/components/InstructionPage/InstructionPage.js
+++ client/src/components/InstructionPage/InstructionPage.js
@@ -6,14 +6,14 @@ export default function InstructionPage() {
     const navigate = useNavigate();
 
     return (
-        <div className="Home">
+        <div className="d-flex flex-column d-flex align-items-center">
             <h1>
                 How to Play:
             </h1>
             <p>
                 This is how you play.
             </p>
-            <button onClick={() => navigate('/')}>
+            <button className="btn btn-primary m-2" onClick={() => navigate('/')}>
                 back
             </button>           
         </div>
