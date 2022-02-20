406a1138edb51f7878935aba377d230be9424ce5
Aleck Bowen Sun <aleck.bowen.sun@gmail.com>
2022-02-19 22:58:05 -0500

    Merge conflicts

--- client/src/components/HomePage/Home.js
+++ client/src/components/HomePage/Home.js
@@ -7,21 +7,21 @@ export default function HomePage() {
     const navigate = useNavigate();
     
     return (
-        <div className="d-flex flex-column align-items-center">
+        <div className="HomePage d-flex flex-column align-items-center container">
             <h1>
                 Juiced.io
             </h1>
             <p>Battle your friends and compete for title of best athlete!</p>
             <img src={homeImg} />
             <div className="d-flex justify-content-center">
-                <button className="btn btn-primary m-2" onClick={() => navigate('/CreateRoom')}>
+                <button className="btn btn-dark m-2" onClick={() => navigate('/CreateRoom')}>
                     Create Game
                 </button>
-                <button className="btn btn-primary m-2" onClick={() => navigate('/JoinRoom')}>
+                <button className="btn btn-dark m-2" onClick={() => navigate('/JoinRoom')}>
                     Join Game
                 </button> 
             </div>
-            <button className="btn btn-primary m-2" onClick={() => navigate('/instructions')}>
+            <button className="btn btn-danger m-2" onClick={() => navigate('/instructions')}>
                     How do I play?
             </button>           
         </div>
--- client/src/components/WaitRoom/WaitRoom.js
+++ client/src/components/WaitRoom/WaitRoom.js
@@ -1,31 +1,45 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
+import group_7Img from '../../images/Group_7.png';
 import "../../styles.css";
 
 export default function WaitRoom() {
     const navigate = useNavigate();
-    
+
     return (
-        <div className="WaitRoom">
-            <h1>
-                Welcome to the waiting room!
-            </h1>
-            <div className="players">
-                <ul>
-                    <li>
-                        Elena
-                    </li>
-                    <li>
-                        Fred
-                    </li>
-                    <li>
-                        Gavin
-                    </li>
-                </ul>
+        <div className="WaitRoom container d-flex justify-content-center">
+            <div className="row">
+                <div className="col-ms-12 text-center">
+                    <h1>
+                        Welcome to the waiting room!
+                    </h1>
+                    <img src={group_7Img} />
+                </div>
+                <div className="players col-ms-12">
+                    <div className="card-deck">
+                        <div className="card mb-3">
+                            <div className="card-body">
+                                Elena
+                            </div>
+                        </div>
+                        <div className="card mb-3">
+                            <div className="card-body">
+                                Fred
+                            </div>
+                        </div>
+                        <div className="card mb-3">
+                            <div className="card-body">
+                                Gavin
+                            </div>
+                        </div>                       
+                    </div>       
+                </div>
+                <div className="col-ms-12">
+                    <button className="btn btn-dark btn-lg" onClick={() => navigate('/Workouts')}>
+                        Start Game
+                    </button>                    
+                </div>
             </div>
-            <button onClick={() => navigate('/Workouts')}>
-                Start Game
-            </button>
         </div>
     );
 }
