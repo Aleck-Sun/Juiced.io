9e7d2e7654d00653064d2cf6a2afec1b1e3e95be
Aleck Bowen Sun <aleck.bowen.sun@gmail.com>
2022-02-20 11:46:20 -0500

    Fix bug

--- client/src/components/WinnersPage/WinnersPage.js
+++ client/src/components/WinnersPage/WinnersPage.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import fitz_RunningImg from '../../images/Fitz_Running.png';
 
 import "../../styles.css";
