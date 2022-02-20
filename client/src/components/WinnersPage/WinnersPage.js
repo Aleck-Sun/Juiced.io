4f4296efba5be5925a7eb4f46c57a951eb11fd27
fengchiw <wilsonfwang@gmail.com>
2022-02-20 11:04:50 -0500

    winners

--- client/src/components/WinnersPage/WinnersPage.js
+++ client/src/components/WinnersPage/WinnersPage.js
@@ -21,11 +21,11 @@ export default function WinnersPage() {
         <div className="WinnersPage">
             <h3>Results!</h3>
             <div className="d-flex flex-column align-items-center container">
-                    {winnersList.length > 0 ? winnersList.map((player, index) => {
+                    {winnersList.length > 0 ? winnersList.map((player) => {
                     return <>
                                 <div className="card mb-3" key={player.user}>
                                     <div className="card-body" key={player.user}>
-                                        {index}: {player.user} who scored {player.points} points
+                                         {player.user} who scored {player.points} points
                                     </div>
                                 </div>
                             </>
