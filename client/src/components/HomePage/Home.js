1409695fa9a9eee5c300a6ba95aceb6dc5e79bba
arakush <arakush@sfu.ca>
2022-02-19 16:08:57 -0800

    Edited the styles.css of the components

--- client/src/components/HomePage/Home.js
+++ client/src/components/HomePage/Home.js
@@ -7,20 +7,20 @@ export default function HomePage() {
     const navigate = useNavigate();
     
     return (
-        <div className="d-flex flex-column d-flex align-items-center">
+        <div className="HomePage d-flex flex-column align-items-center container">
             <h1>
                 Ready to battle?
             </h1>
             <img src={homeImg} />
             <div className="d-flex justify-content-center">
-                <button className="btn btn-primary m-2" onClick={() => navigate('/CreateRoom')}>
+                <button className="btn btn-dark m-2" onClick={() => navigate('/CreateRoom')}>
                     Create Game
                 </button>
-                <button className="btn btn-primary m-2" onClick={() => navigate('/JoinRoom')}>
+                <button className="btn btn-dark m-2" onClick={() => navigate('/JoinRoom')}>
                     Join Game
                 </button> 
             </div>
-            <button className="btn btn-primary m-2" onClick={() => navigate('/instructions')}>
+            <button className="btn btn-danger m-2" onClick={() => navigate('/instructions')}>
                     How do I play?
             </button>           
         </div>
--- client/src/components/WelcomePage/WelcomePage.js
+++ client/src/components/WelcomePage/WelcomePage.js
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import homeImg from '../../images/home.png';
+import fitz_Strength_TrainingImg from '../../images/Fitz_Strength_Training.png';
 import "../../styles.css";
 
 export default function WelcomePage() {
     const navigate = useNavigate();
     
     return (
-        <div className="d-flex flex-column d-flex align-items-center">
+        <div className="WelcomePage d-flex flex-column align-items-center container">
             <h1>
                 Welcome!
             </h1>
             <h3>
                 Don't forget to print out your QR code.
             </h3>
-            <img src={homeImg} />
+            <img src={fitz_Strength_TrainingImg} />
             <div className="d-flex justify-content-center">
-                <button className="btn btn-primary m-2">
+                <button className="btn btn-dark m-2">
                     Print QR Code
                 </button>
-                <button className="btn btn-primary m-2" onClick={() => navigate('/home')}>
+                <button className="btn btn-dark m-2" onClick={() => navigate('/home')}>
                     Start Playing
                 </button> 
             </div>
-            <button className="btn btn-primary m-2" onClick={() => navigate('/instructions')}>
+            <button className="btn btn-danger m-2" onClick={() => navigate('/instructions')}>
                     How do I play?
             </button>           
         </div>
